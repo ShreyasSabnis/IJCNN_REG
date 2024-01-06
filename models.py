b5ef0e8bf7753a4f77e42c4ebc87f89a982134f8
RaghavSinghal-10 <19d070049@iitb.ac.in>
2024-01-06 14:01:35 +0530

    addes stuff

--- models.py
+++ models.py
@@ -5,17 +5,51 @@ import snntorch as snn
 from snntorch import surrogate, utils
 from collections import OrderedDict
 
+class SNN_Vanilla(nn.Module):
+    def __init__(self, beta, learn_beta, threshold, learn_threshold):
+        super().__init__()
+
+        self.spike_grad = surrogate.atan()
+        self.beta = beta
+        self.learn_beta = learn_beta
+        self.learn_threshold = learn_threshold
+
+        self.net = nn.Sequential(
+                    nn.Flatten(),
+                    nn.Linear(34*34*2, 1000),
+                    snn.Leaky(beta=beta, spike_grad=self.spike_grad, init_hidden=True, learn_beta=self.learn_beta),
+                    nn.Linear(1000, 10),
+                    snn.Leaky(beta=beta, spike_grad=self.spike_grad, init_hidden=True, learn_beta=self.learn_beta, output=True)
+                    )
+
+
+    def forward(self, data):
+        spk_rec = []
+        mem_rec = []
+        utils.reset(self.net)  # resets hidden states for all LIF neurons in net
+
+        #print(data.size(1))
+        for step in range(data.size(1)):  # data.size(1) = number of time steps
+            spk_out, mem_out = self.net(data[:,step,:,:,:])
+            #spk_out, mem_out = self.net(data[step])
+            spk_rec.append(spk_out)
+            mem_rec.append(mem_out)
+            #print(spk_out.size())
+            
+        return torch.stack(spk_rec), torch.stack(mem_rec)
 
 class SRNN(nn.Module):
     def __init__(self, beta_lsm, beta_lif, learn_beta, threshold, learn_threshold, N, all_to_all, output_size, input_size):
         super().__init__()
 
+        # no need to change these params
         spike_grad = surrogate.atan()
         self.learn_beta = learn_beta
         self.learn_threshold = learn_threshold
+        self.threshold = threshold
+
         self.beta_lsm = beta_lsm
         self.beta_lif = beta_lif
-        self.threshold = threshold
         self.N = N
         self.all_to_all = all_to_all
         self.output_size = output_size
@@ -52,6 +86,7 @@ class SRNN(nn.Module):
             
 
     def forward(self, data):
+
         spk_rec = []
         mem_rec = []
 
@@ -60,58 +95,125 @@ class SRNN(nn.Module):
 
         # do not compute gradient for first 20% time steps
 
+        # data is of size [batch_size, time_steps, 2, 34, 34]
         with torch.no_grad():
+
             for step in range(int(data.size(1)*0.2)):
+
                 in_curr = self.fc1(self.flatten(data[:,step,...]))
                 spk_lsm, mem_lsm = self.lsm(in_curr, spk_lsm, mem_lsm) 
                 out_curr = self.fc2(spk_lsm)
                 spk_out, mem_out = self.lif1(out_curr, mem_out)
+
                 spk_rec.append(spk_out)
                 mem_rec.append(mem_out)
 
         # compute gradient for remaining 80% time steps
         for step in range(int(data.size(1)*0.2), data.size(1)):
+
             in_curr = self.fc1(self.flatten(data[:,step,...]))
             spk_lsm, mem_lsm = self.lsm(in_curr, spk_lsm, mem_lsm)
             out_curr = self.fc2(spk_lsm)
             spk_out, mem_out = self.lif1(out_curr, mem_out)
+
             spk_rec.append(spk_out)
             mem_rec.append(mem_out)
 
 
         return torch.stack(spk_rec), torch.stack(mem_rec)
-    
 
+# output is of size [time_steps, batch_size, num_classes]
+class SCRNN(nn.Module):
 
-class SNN_Vanilla(nn.Module):
-    def __init__(self, beta, learn_beta, threshold, learn_threshold):
+    # input size if either h or w of input
+
+    def __init__(self, beta_lsm, beta_lif, learn_beta, threshold, learn_threshold, all_to_all, input_size, num_classes):
+        
         super().__init__()
 
-        self.spike_grad = surrogate.atan()
-        self.beta = beta
+        # no need to change these params
+        spike_grad = surrogate.atan()
         self.learn_beta = learn_beta
         self.learn_threshold = learn_threshold
+        self.threshold = threshold
 
-        self.net = nn.Sequential(
-                    nn.Flatten(),
-                    nn.Linear(34*34*2, 1000),
-                    snn.Leaky(beta=beta, spike_grad=self.spike_grad, init_hidden=True, learn_beta=self.learn_beta),
-                    nn.Linear(1000, 10),
-                    snn.Leaky(beta=beta, spike_grad=self.spike_grad, init_hidden=True, learn_beta=self.learn_beta, output=True)
-                    )
+        self.beta_lsm = beta_lsm
+        self.beta_lif = beta_lif
+        self.all_to_all = all_to_all
+        self.num_classes = num_classes
+        self.input_size = input_size
 
+        self.conv1 = nn.Conv2d(2, 32, kernel_size=3, stride=1, padding=1)
+        self.mp1 = nn.MaxPool2d(2)
+        self.ln1 = nn.LayerNorm([32, input_size//2, input_size//2], elementwise_affine=False)
+        self.lif1 = snn.Leaky(beta=self.beta_lif, spike_grad=spike_grad, learn_beta=self.learn_beta, threshold=self.threshold,
+                              learn_threshold=self.learn_threshold)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.mp2 = nn.MaxPool2d(2)
+        self.ln2 = nn.LayerNorm([64, input_size//4, input_size//4], elementwise_affine=False)
+        self.lif2 = snn.Leaky(beta=self.beta_lif, spike_grad=spike_grad, learn_beta=self.learn_beta, threshold=self.threshold,
+                              learn_threshold=self.learn_threshold)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.mp3 = nn.MaxPool2d(2)
+        self.ln3 = nn.LayerNorm([128, input_size//8, input_size//8], elementwise_affine=False)
+        self.lif3 = snn.Leaky(beta=self.beta_lif, spike_grad=spike_grad, learn_beta=self.learn_beta, threshold=self.threshold,
+                              learn_threshold=self.learn_threshold)
+        
+        self.flatten = nn.Flatten()
+
+        self.rsnn = snn.RLeaky(beta=self.beta_lsm, all_to_all=self.all_to_all, spike_grad=spike_grad, learn_beta=self.learn_beta,
+                                    learn_threshold=self.learn_threshold, linear_features=128*(input_size//8)*(input_size//8), threshold=self.threshold)
+        
+        self.fc_out = nn.Linear(128*(input_size//8)*(input_size//8), num_classes)
+        self.lif_out = snn.Leaky(beta=self.beta_lif, spike_grad=spike_grad, learn_beta=self.learn_beta, output=True)
+            
 
     def forward(self, data):
+
         spk_rec = []
         mem_rec = []
-        utils.reset(self.net)  # resets hidden states for all LIF neurons in net
 
-        #print(data.size(1))
-        for step in range(data.size(1)):  # data.size(1) = number of time steps
-            spk_out, mem_out = self.net(data[:,step,:,:,:])
-            #spk_out, mem_out = self.net(data[step])
+        mem_1 = self.lif1.init_leaky()
+        mem_2 = self.lif2.init_leaky()
+        mem_3 = self.lif3.init_leaky()
+    
+        spk_rsnn, mem_rsnn = self.rsnn.init_rleaky()
+        mem_out = self.lif_out.init_leaky()
+
+        # data is of size [batch_size, time_steps, c, h, w]
+
+        # do not compute gradient for first 20% time steps
+        with torch.no_grad():
+
+            for step in range(int(data.size(1)*0.2)):
+
+                print(data.size())
+
+                spk_1, mem_1 = self.lif1(self.ln1(self.mp1(self.conv1(data[:,step,:,:,:]))), mem_1)
+                spk_2, mem_2 = self.lif2(self.ln2(self.mp2(self.conv2(spk_1))), mem_2)
+                spk_3, mem_3 = self.lif3(self.ln3(self.mp3(self.conv3(spk_2))), mem_3)
+
+                spk_rsnn, mem_rsnn = self.rsnn(self.flatten(spk_3), spk_rsnn, mem_rsnn)
+
+                spk_out, mem_out = self.lif_out(self.fc_out(spk_rsnn), mem_out)
+
+                spk_rec.append(spk_out)
+                mem_rec.append(mem_out)
+
+        # compute gradient for remaining 80% time steps
+        for step in range(int(data.size(1)*0.2), data.size(1)):
+
+            spk_1, mem_1 = self.lif1(self.ln1(self.mp1(self.conv1(data[:,step,:,:,:]))), mem_1)
+            spk_2, mem_2 = self.lif2(self.ln2(self.mp2(self.conv2(spk_1))), mem_2)
+            spk_3, mem_3 = self.lif3(self.ln3(self.mp3(self.conv3(spk_2))), mem_3)
+
+            spk_rsnn, mem_rsnn = self.rsnn(self.flatten(spk_3), spk_rsnn, mem_rsnn)
+
+            spk_out, mem_out = self.lif_out(self.fc_out(spk_rsnn), mem_out)
+
             spk_rec.append(spk_out)
             mem_rec.append(mem_out)
-            #print(spk_out.size())
-            
+
+        print(torch.stack(spk_rec).size())
+        exit()
         return torch.stack(spk_rec), torch.stack(mem_rec)
--- train.py
+++ train.py
@@ -15,9 +15,10 @@ from ray import tune
 from ray.air import Checkpoint, session
 from ray.tune.schedulers import ASHAScheduler
 
-# import wandb
+import wandb
 
-# wandb.init()
+wandb.init(project='ijcnn reg')
+
 parser = argparse.ArgumentParser(description='Spiking RNN training')
 
 
@@ -36,32 +37,34 @@ parser.add_argument('--beta_lsm', default=0.5, type=float, help='beta for the LI
 parser.add_argument('--beta_lif', default=0.5, type=float, help='beta for the LIF neurons in the backbone')
 parser.add_argument('--time_steps', default=150, type=int, help='number of time steps for the SNN')
 parser.add_argument("--all_to_all", action="store_true", help="Use all-to-all connectivity in the LSM layer")
-parser.add_argument("--use_l1_loss", action="store_true", help="Use l1 loss for the lsm layer")
+parser.add_argument("--use_reg_loss", action="store_true", help="Use l1 loss for the lsm layer")
 parser.add_argument("--lambda_l1", default=0.01, type=float, help="lambda for l1 loss")
 parser.add_argument('--learn_beta', action='store_true', help='learn beta for the LIF neurons in the backbone')
-
+parser.add_argument('--dataset', default='NMNIST', type=str, help='dataset to be used for training the linear classifier')
 
 args = parser.parse_args()
 
-# wandb.config.update(args)
+wandb.config.update(args)
 
 def main():
 
     device = torch.device(args.device if torch.cuda.is_available() else "cpu")
 
-    SRNN_model = SRNN(beta_lsm=args.beta_lsm, beta_lif=args.beta_lif, learn_beta=args.learn_beta, threshold=args.threshold, learn_threshold=args.learn_threshold, 
-                        N=args.N, all_to_all=args.all_to_all).to(device)
+    SRNN_model = SCRNN(beta_lsm=args.beta_lsm, beta_lif=args.beta_lif, learn_beta=args.learn_beta, threshold=args.threshold, 
+                       learn_threshold=args.learn_threshold, all_to_all=args.all_to_all, num_classes=10, input_size=34).to(device)
 
-    sensor_size = tonic.datasets.NMNIST.sensor_size
+    # dataloading
+    if args.dataset == "NMNIST":
 
-    transform = tonic.transforms.Compose([
-            tonic.transforms.Denoise(filter_time=10000),
-            tonic.transforms.ToFrame(sensor_size=sensor_size, n_time_bins=args.time_steps)
-            ])
+        sensor_size = tonic.datasets.NMNIST.sensor_size
 
-    trainset = tonic.datasets.NMNIST(save_to='./data', train=True, transform=transform)
-    testset = tonic.datasets.NMNIST(save_to='./data', train=False, transform=transform)
+        transform = tonic.transforms.Compose([
+                tonic.transforms.Denoise(filter_time=10000),
+                tonic.transforms.ToFrame(sensor_size=sensor_size, n_time_bins=args.time_steps)
+                ])
 
+        trainset = tonic.datasets.NMNIST(save_to='./data', train=True, transform=transform)
+        testset = tonic.datasets.NMNIST(save_to='./data', train=False, transform=transform)
     
     print("trainset: ", len(trainset))
     print("testset: ", len(testset))
@@ -69,18 +72,20 @@ def main():
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch_size_train, shuffle=True, num_workers=args.num_workers, drop_last=False)
     testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch_size_test, shuffle=False, num_workers=args.num_workers, drop_last=False)
 
-    #implement a learning rate scheduler
-
     optimizer = torch.optim.Adam(SRNN_model.parameters(), lr=args.lr, betas=(0.9, 0.999))
-    #scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.95)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.num_epochs)
     criterion = SF.mse_count_loss(correct_rate=0.8, incorrect_rate=0.2)
 
     loss_history = []
     loss_total_history = []
-    accuracy_history = []
+    accuracy_test_history = []
 
+    SRNN_model.train()
 
     for epoch in range(args.num_epochs):  # loop over the dataset multiple times
+        
+        # running loss is normal loss
+        # running loss total is normal + reg loss
 
         running_loss = 0.0
         running_loss_total = 0.0
@@ -94,8 +99,6 @@ def main():
 
             input = input.type(torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor)
 
-            SRNN_model.train()
-
             spk_rec, mem_rec = SRNN_model(input)
 
             loss = criterion(spk_rec, labels)
@@ -103,11 +106,8 @@ def main():
             # extract weights of the lsm layer in the SRNN model
             lsm_weights = SRNN_model.lsm.recurrent.weight
             
-            if args.use_l1_loss:
-                # add l1 loss of the lsm weights which are in the form of a matrix
-                l1_reg = torch.norm(lsm_weights, 1)
-
-                total_loss = loss + args.lambda_l1*l1_reg
+            if args.use_reg_loss:
+                total_loss = loss
             else:
                 total_loss = loss
           
@@ -118,36 +118,34 @@ def main():
             optimizer.step()
 
             # print statistics
-            running_loss += loss.item()
-            running_loss_total += total_loss.item()
+            running_loss += loss.detach().cpu.item()
+            running_loss_total += total_loss.detach().cpu().item()
 
-        #scheduler.step()
+            wandb.log({"loss": loss.detach().cpu().item(), "total_loss": total_loss.detach().cpu().item()})
 
-        # calculate % of non-zero weights in the lsm layer
+        scheduler.step()
+
+        # calculate % of non-zero weights in the lsm layer and print sparsity
         lsm_weights = SRNN_model.lsm.recurrent.weight
         non_zero = torch.nonzero(lsm_weights)
         non_zero = len(non_zero)
         total = lsm_weights.numel()
         sparsity = 100*(1 - (non_zero/total))
 
-        #print epoch loss
+        print("Sparsity: ", sparsity)
+
+        #print epoch loss and total loss
         loss_history.append(running_loss / len(trainloader))
         loss_total_history.append(running_loss_total / len(trainloader))
         print('[%d] loss: %.3f' % (epoch + 1, running_loss / len(trainloader)))
         print('[%d] loss_total: %.3f' % (epoch + 1, running_loss_total / len(trainloader)))
 
-        # print the new value of beta in the lsm layer
-        print("Beta of lsm: ", SRNN_model.lsm.beta.item())
-
-        # print the new value of beta in the lif layer
-        print("Beta of lif: ", SRNN_model.lif1.beta.item())
-
-        print("Sparsity: ", sparsity)
-
-        total = 0
-        running_acc = 0.0
+        SRNN_model.eval()
 
         with torch.no_grad():
+
+            correct = 0
+            total = 0
 
             for i, (images, labels) in enumerate(testloader, 0):
                 
@@ -158,19 +156,22 @@ def main():
 
                 images = images.type(torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor)
 
-                SRNN_model.eval()
-
                 spk_rec, mem_rec = SRNN_model(images)
 
-                acc = SF.accuracy_rate(spk_rec, labels)*100
+                # calculate number of correct predictions
+                _, idx = spk_rec.sum(dim=0).max(1)
+                correct += idx.eq(labels).sum().item()
 
                 total += labels.size(0)
-                running_acc += acc
 
-            accuracy = running_acc / len(testloader)
-            accuracy_history.append(accuracy)
+            accuracy_test = 100. * correct / total
+            accuracy_test_history.append(accuracy_test)
+            max_accuracy = max(accuracy_test_history)
 
-            print("Total: {}, Accuracy: {}".format(total, accuracy))
+            wandb.log({"test_acc": accuracy_test, "max_test_acc": max_accuracy})
+
+            print('Test accuracy: %.3f %%' % (accuracy_test))
+            print('Max test accuracy: %.3f %%' % (max_accuracy))
           
 if __name__ == '__main__':
     main()
